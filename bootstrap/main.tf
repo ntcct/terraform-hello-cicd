8c298f05e644e81f7b4e83a951bccb8fc782d4e2
Thi Nguyen <ntcct@yahoo.com>
2026-06-25 10:04:30 +1000

    Grant iam:ListInstanceProfilesForRole to CI role (needed to delete IAM roles)

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -151,6 +151,7 @@ data "aws_iam_policy_document" "github_permissions" {
       "iam:DetachRolePolicy",
       "iam:ListRolePolicies",
       "iam:ListAttachedRolePolicies",
+      "iam:ListInstanceProfilesForRole",
       "iam:GetPolicy",
     ]
     resources = ["*"]
